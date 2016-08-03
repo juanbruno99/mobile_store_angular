844c9db188d05bc254af28d3bb215e4b44ca5845
Juan Bruno MacBook <juan.marcos.bruno@gmail.com>
2016-08-03 22:12:01 +0100

    Second Commit: - Addition of controller to main module, with the built in $scope service injected and having some objects from the model - This in conjunction with the html code produces the first functional version of this webapp

---
+++ js/controllers/MainController.js
@@ -0,0 +1,23 @@
+//Main Controller definition, for now with it's built in $scope service containing model objs
+app.controller('MainController', ['$scope', function($scope) {
+    $scope.move = {
+    icon: 'img/move.jpg',
+    title: 'MOVE',
+    developer: 'MOVE, Inc.',
+    price: 0.99
+  };
+
+  $scope.shutterbugg = {
+    icon: 'img/shutterbugg.jpg',
+    title: 'Shutterbugg',
+    developer: 'Chico Dusty',
+    price: 2.99
+  };
+
+  $scope.gameboard = {
+    icon: 'img/gameboard.jpg',
+    title: 'Gameboard',
+    developer: 'Armando P.',
+    price: 1.99
+  };
+}]);
